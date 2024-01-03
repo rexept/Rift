27892cb5a928937737e333c55385e9e64afb32c2
SDB-Industries <sdbindustries@protonmail.com>
2024-01-03 15:56:37 -0700

    Added status and other options.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use args::{RiftArgs, EntityType, OutputSubcommand};
+use args::{EntityType, OutputSubcommand, RiftArgs};
 use clap::Parser;
 
 pub mod args;
@@ -12,7 +12,12 @@ async fn main() -> Result<()> {
     let args = RiftArgs::parse();
 
     match args.entity_type {
-        EntityType::Daemon => socket::connect_to_hyprland_socket().await?,
+        EntityType::Daemon(daemon) => match daemon.command {
+            args::DaemonSubcommand::Start => socket::connect_to_hyprland_socket().await?,
+            args::DaemonSubcommand::Stop => socket::disconnect_from_hyprland_socket().await,
+            args::DaemonSubcommand::Restart => socket::restart().await?,
+            args::DaemonSubcommand::Status => socket::status().await?,
+        },
         EntityType::Menu => eww::open_eww_widget().await?,
         EntityType::Output(output) => match output.command {
             OutputSubcommand::Mirror => monitor::mirror()?,
